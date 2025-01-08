6365a820ff039234c1751f6f50f02a778657e269
nikololiahim <olomishcak@outlook.com>
2025-01-08 21:00:14 +0500

    remove stuff i don't use (yet)
    sorry Vlad :(

--- flake.nix
+++ flake.nix
@@ -3,50 +3,49 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    nixpkgsForGraal.url = "github:nixos/nixpkgs?rev=1939434b4ae04cb855edec936573c778a9ddeab0";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgsForGraal,
-    flake-utils,
-  }: let
-    supportedSystems = [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-linux"
-      "x86_64-darwin"
-    ];
-  in
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    let
+      supportedSystems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+        "x86_64-darwin"
+      ];
+    in
     flake-utils.lib.eachSystem supportedSystems (
-      system: let
-        pkgs = import ./pkgs.nix nixpkgs nixpkgsForGraal system;
+      system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = false;
+        };
 
-        makeShell = p:
+        makeShell =
+          p:
           p.mkShell {
             buildInputs = with p; [
-              ammonite
-              bloop
-              coursier
+              nixfmt-rfc-style
+              nixd
               jdk
-              mill
-              sbt
               scala-cli
               scalafmt
             ];
           };
-      in {
+      in
+      {
         devShells = {
-          default = makeShell pkgs.default;
-          java21 = makeShell pkgs.pkgs21;
-          java17 = makeShell pkgs.pkgs17;
-          java11 = makeShell pkgs.pkgs11;
-          java8 = makeShell pkgs.pkgs8;
+          default = makeShell pkgs;
         };
 
-        formatter = pkgs.default.alejandra;
+        formatter = pkgs.nixfmt-rfc-style;
       }
     );
 }
